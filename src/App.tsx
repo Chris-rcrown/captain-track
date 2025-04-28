e3359e161e8fb2c16424912b75c306216a68694c
Rotimi Crown <rotimiworkmail1@gmail.com>
2025-04-28 14:06:13 +0100

    changes made on images and app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
-import Hero from './components/Hero';
-import Section1 from './components/Section1';
-import Services from './components/Services';
-import Section3 from './components/Section3';
-import Section4 from './components/Section4';
-import Footer from './components/Footer';
+import Hero from './components/hero';
+import Section1 from './components/section1';
+import Services from './components/services';
+import Section3 from './components/section3';
+import Section4 from './components/section4';
+import Footer from './components/footer';
 
 const App: React.FC = () => {
   return (
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../design/CaptainTrack (1) (1) 2.png';
+import Logo from '../design/CaptainTrack2.png'
 import { NavLink, Link } from 'react-router-dom';
 import { Icon } from '@iconify/react';
 import { companyLocation, contactSupport, footerNav } from '../data/footer';
